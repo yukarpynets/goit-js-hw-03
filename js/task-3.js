577fb74ae09f73d0eab0200935daf8c3bf2846ba
Yulia Karpynets <yukarpynets90@gmail.com>
2026-02-11 11:43:36 +0100

    HW3

---
+++ js/task-3.js
@@ -0,0 +1,15 @@
+function filterArray(numbers, value) {
+  const newArr= [];
+  for (const number of numbers) {
+    if (number > value) {
+      newArr.push(number);
+    }
+  }
+  return newArr;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
